4c7c23ff5a4f046a0ec55c4d59433b22e7310cfb
onenewcode <lspneverstudy@gmail.com>
2025-09-30 22:29:10 +0800

    修复警告

--- service/tests/prepare.rs
+++ service/tests/prepare.rs
@@ -1,7 +1,7 @@
+#![cfg(feature = "mock")]
 use ::entity::post;
 use sea_orm::*;
 
-#[cfg(feature = "mock")]
 pub fn prepare_mock_db() -> DatabaseConnection {
     MockDatabase::new(DatabaseBackend::Postgres)
         .append_query_results([
